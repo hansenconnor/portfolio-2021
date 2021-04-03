f1268171c8b8bfe8fee6e983d288a107f038baee
Connor Hansen <connorhnsn5596@gmail.com>
2021-04-02 23:22:59 -0400

    Cleanup layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import Navbar from "./navbar"
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Scrollbar from 'smooth-scrollbar'
-// import "../styles/locomotive-scroll.css"
 import Scroll from './locomotive-scroll'
 import scrollbarStyles from '../styles/smooth-scrollbar.css'
 
@@ -11,41 +10,6 @@ import scrollbarStyles from '../styles/smooth-scrollbar.css'
 const Layout = ({ children, location }) => {
   
   const scrollerRef = useRef()
-
-  // useEffect(() => {
-
-  //   console.log('initializing layout...');
-    
-  //   gsap.registerPlugin(ScrollTrigger)
-    
-  //   const scroller = scrollerRef.current
-
-  //   const bodyScrollBar = Scrollbar.init(scrollerRef.current, { damping: 0.1, delegateTo: document, alwaysShowTracks: true })
-
-  //   window.bodyScrollBar = bodyScrollBar
-
-  //   ScrollTrigger.scrollerProxy(scrollerRef.current, {
-  //     scrollTop(value) {
-  //       if (arguments.length) {
-  //         bodyScrollBar.scrollTop = value
-  //       }
-  //       return bodyScrollBar.scrollTop
-  //     }
-  //   })
-
-  //   window.scroller = scrollerRef.current
-
-  //   ScrollTrigger.refresh()
-  //   bodyScrollBar.addListener(ScrollTrigger.update)
-
-  //   ScrollTrigger.defaults({ scroller: scroller })
-
-    
-  //   return () => {
-  //     if (bodyScrollBar) bodyScrollBar.destroy()
-  //     bodyScrollBar.removeListener(ScrollTrigger.update)
-  //   }
-  // }, [location])
   
   return (
     <div className="global-wrapper scroller h-screen overflow-hidden" ref={scrollerRef}>
